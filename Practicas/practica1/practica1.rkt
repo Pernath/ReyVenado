23e29324a56c0ca31c212b0627a19cd99c95e646
andreagonz <andreagonz@ciencias.unam.mx>
2015-09-01 23:45:54 -0500

    primes agregado

--- Practicas/practica1/practica1.rkt
+++ Practicas/practica1/practica1.rkt
@@ -11,16 +11,36 @@
       [(eq? b 1) a]
       [else (* a (pow a (- b 1)))]))
             
-;Average function.
+;Auxiliary function.
 (define (average_aux lst t l)
     (cond
       [(empty? lst) (floor (/ t l))]
       [else (average_aux (cdr lst) (+ (car lst) t) (+ l 1))]))
-      
+
+;Average function.
 (define (average lst)
     (cond
       [(empty? lst) 0]
       [else (average_aux lst 0 0)]))
+
+;Says if a number is prime
+(define (is-prime a b)
+    (cond
+      [(< a 2) #f]
+      [(eq? a b) #t]
+      [(zero? (modulo a b)) #f ]
+      [else (is-prime a (+ b 1))]))
+
+;Auxiliary function
+(define (pprimes a b)
+  (cond
+    [(eq? b (+ a 1)) '()]
+    [(is-prime b 2) (cons b (pprimes a (+ b 1)))]
+    [else (pprimes a (+ b 1))]))
+
+;Returns a list of prime numbers up to a given number
+(define (primes a)
+    (pprimes a 0))
 
 ;Zip function (not ready, yet)
 (define (zip lst1 lst2) ;recieves two lists as parameters
@@ -69,6 +89,10 @@
 (test (average '(3)) 3)
 (test (average '(2 3 2 3 5 1 9)) 3)
 (test (average '(3 4 3 5 5 12)) 5)
+(test (primes 1) '())
+(test (primes 2) '(2))
+(test (primes 11) '(2 3 5 7 11))
+(test (primes 41) '(2 3 5 7 11 13 17 19 23 29 31 37 41))
 (test (zip '() '()) '())
 (test (zip '(1 2 3) '(1 2 3)) '((1 1) (2 2) (3 3)))
 (test (zip '(1 2 3) '(1 2)) '((1 1) (2 2)))
