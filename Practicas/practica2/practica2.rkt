132939ae375c46ecf1a9b8839ae5e188dede899d
Pernath <cgah.95@gmail.com>
2015-09-11 01:57:52 -0500

    doc

--- Practicas/practica2/practica2.rkt
+++ Practicas/practica2/practica2.rkt
@@ -1,1 +1,11 @@
 #lang plai
+
+;;; Laboratorio de Lenguajes de Programación ;;;
+;;;;;;;;;;;;;;;;;;; Práctica 2 ;;;;;;;;;;;;;;;;;
+
+;;;;;;;Integrantes;;;;;;;;
+;;;;; Andrea González ;;;;
+;;;;; Karla Esquivel ;;;;;
+;;;;; Carlos Acosta ;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
