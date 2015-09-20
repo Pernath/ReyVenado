abdac6ced8701bfd6e40551beb3e16d3bba15a02
andreagonz <andreagonz@ciencias.unam.mx>
2015-09-19 19:01:21 -0500

    zones

--- Practicas/practica3/practica3.rkt
+++ Practicas/practica3/practica3.rkt
@@ -1,3 +1,19 @@
 #lang plai
 
 (require "practica3-base.rkt")
+
+(define (zones rheart-rate mheart-rate)  
+  (define mrange (- mheart-rate rheart-rate))
+  (list
+      (resting rest (- (* rheart-rate 0.5) 1))
+      (warm-up (aux-min rheart-rate mrange 1) (aux-max rheart-rate mrange 1))
+      (fat-burning  (aux-min rheart-rate mrange 2) (aux-max rheart-rate mrange 2))
+      (aerobic (aux-min rheart-rate mrange 3) (aux-max rheart-rate mrange 3))
+      (anaerobic  (aux-min rheart-rate mrange 4) (aux-max rheart-rate mrange 4))
+      (maximum (aux-max rheart-rate mrange 5) (aux-max rheart-rate mrange 5))))
+
+(define (aux-min rheart-rate mrange i)
+    (+ rheart-rate (* mrange (+ 0.5 (* 0.1 (- i 1))))))
+
+(define (aux-max rheart-rate mrange i)
+    (- (+ rheart-rate (* mrange (+ 0.5 (* 0.1 i)))) 1))
