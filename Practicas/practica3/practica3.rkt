54bc623d791318cee027b68d1e3a43faa4211228
Pernath <cgah.95@gmail.com>
2015-09-19 20:10:56 -0500

    correccion del resting... otra vez

--- Practicas/practica3/practica3.rkt
+++ Practicas/practica3/practica3.rkt
@@ -5,7 +5,7 @@
 (define (zones rheart-rate mheart-rate)  
   (define mrange (- mheart-rate rheart-rate)) ;; esta es la linea que debemos modificar? cómo debemos hacerlo?
   (list
-      (resting rheart-rate (+ rheart-rate (- (* rheart-rate 0.5) 1)))
+      (resting rheart-rate (+ rheart-rate (- (* mrange 0.5) 1)))
       (warm-up (aux-min rheart-rate mrange 1) (aux-max rheart-rate mrange 1))
       (fat-burning  (aux-min rheart-rate mrange 2) (aux-max rheart-rate mrange 2))
       (aerobic (aux-min rheart-rate mrange 3) (aux-max rheart-rate mrange 3))
