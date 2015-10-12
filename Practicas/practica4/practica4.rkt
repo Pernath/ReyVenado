34e6aadb0244523bd7ea9fa4371471e02dfce840
andreagonz <andreagonz@ciencias.unam.mx>
2015-10-12 01:14:52 -0500

    more

--- Practicas/practica4/practica4.rkt
+++ Practicas/practica4/practica4.rkt
@@ -36,3 +36,29 @@
    [funS (p b) (fun p (desugar b))]
    [appS (f a) (app (desugar f) (desugarl a))]
    [binopS (f l r) (binop f (desugar l) (desugar r))]))
+
+(test (desugar (parse '{+ 3 4})) (binop + (num 3) (num 4)))
+(test (desugar (parse '{+ {- 3 4} 7})) (binop + (binop - (num 3) (num 4)) (num 7)))
+(test (desugar (parse '{with {{x {+ 5 5}}} x})) (app (fun '(x) (id 'x)) (list (binop + (num 5) (num 5))) ))
+(test (desugar (parse '{with {{x 5} {y 7} {z 2}} {+ 1 2}})) (app (fun '(x y z) (binop + (num 1) (num 2))) (list (num 5) (num 7) (num 2))))
+(test (desugar (parse '{with {{x {* 5 5}} {y 7} {z {/ 10 2}}} {+ x y}})) (app (fun '(x y z) (binop + (id 'x) (id 'y))) (list (binop * (num 5) (num 5)) (num 7) (binop / (num 10) (num 2)))))
+
+(define (cparse sexp)
+  (desugar (parse sexp)))
+
+;; AUXILIARES
+(define (aux-env params args env olenv)
+  (cond
+    [(empty? params) env]
+    [else (aux-env (cdr params) (cdr args) (aSub (car params) (interp (car args) olenv) env) olenv)]))
+
+(define (lookup name env)
+  (type-case Env env
+    [mtSub () (error 'lookup (string-append (~a name) "x symbol is not in the env"))] 
+    [aSub (bound-name bound-value env)
+          (if (symbol=? bound-name name)
+              bound-value
+              (lookup name env))]))
+
+(define (binOp f l r)
+  (numV (f (numV-n l) (numV-n r))))
