8188f1615ca961be466ad26b1265bddfc01c3284
Karla Esquivel <karlycaramelo93@ciencias.unam.mx>
2015-10-12 01:08:48 -0500

    Update practica4.rkt

--- Practicas/practica4/practica4.rkt
+++ Practicas/practica4/practica4.rkt
@@ -8,3 +8,13 @@
   (cond
     [(empty? bs) wexpr]
     [else (withS (list (car bs)) (with-nest (cdr bs) wexpr))]))
+
+(define (desugarl lFAE)
+  (cond
+    [(empty? lFAE) '()]
+    [else (cons (desugar (car lFAE)) (desugarl (cdr lFAE)))]))
+
+(define (desugar-aux lst)
+  (cond
+    [(empty? lst) '()]
+    [else (cons (bind-name (car lst)) (desugar-aux (cdr lst)))]))
