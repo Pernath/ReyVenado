1326778f06612bcf156deddc8b2f9f5a82e70cdd
Karla Esquivel <karlycaramelo93@ciencias.unam.mx>
2015-10-12 01:09:38 -0500

    Update practica4.rkt

--- Practicas/practica4/practica4.rkt
+++ Practicas/practica4/practica4.rkt
@@ -23,3 +23,16 @@
   (cond
     [(empty? lst) '()]
     [else (cons (desugar (bind-val (car lst))) (desugar-aux2 (cdr lst)))]))
+    
+    ;; desugar : FAES -> FAE
+;; takes a FAES expression and transforms it into a FAE expression
+(define (desugar expr)
+  (type-case FAES expr
+   [numS (n) (num n)]
+   [withS (bind body)
+          (app (fun (desugar-aux bind) (desugar body)) (desugar-aux2 bind))]
+   [with*S (bs body) (desugar (with-nest bs body))]
+   [idS (n) (id n)]
+   [funS (p b) (fun p (desugar b))]
+   [appS (f a) (app (desugar f) (desugarl a))]
+   [binopS (f l r) (binop f (desugar l) (desugar r))]))
